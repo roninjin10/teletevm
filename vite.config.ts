b6070c77a01efd7cc63e1a13077c59649869072a
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-08-14 23:45:10 -0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { vitePluginTevm as tevm } from "@tevm/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  build: {
-    outDir: './docs'
-  },
-  base: '/vite-boilerplate/'
+	plugins: [react(), basicSsl(), tevm()],
+	build: {
+		outDir: "./docs",
+	},
+	base: "/vite-boilerplate/",
 });
